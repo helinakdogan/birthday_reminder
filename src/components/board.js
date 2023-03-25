363e9c484fef1ea463eebef072236b17f3b75f9b
Helin Akdoğan <akdoganhelin@gmail.com>
2023-03-25 16:36:29 +0300

    25.03.3023

---
+++ src/components/board.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function board() {
+  return (
+    <main id='site-main'>
+        <h1 className='text-dark title'>Birthday Reminder</h1>
+    </main>
+  )
+}
